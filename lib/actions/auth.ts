951535a7f9ba123a79a0ad2c6b5e0000bc58cdf1
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2025-08-14 13:56:38 +0200

    feat: implement qstash workflows for user onboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "@/lib/workflow";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   credentials: Pick<AuthCredentials, "password" | "email">,
@@ -66,6 +68,14 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
       universityId,
       password: hashedPassword,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     await signInWithCredentials({ email, password });
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "JS Mastery <contact@adrianjsmastery.com>",
+      from: "JS Mastery <hello.rostyslavhorytskyi.online>",
       to: [email],
       subject,
       html: message,
